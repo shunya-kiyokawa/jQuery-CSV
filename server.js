7935b8fa27c789feaa740206ac61b34796a70bfd
crucio1918 <g.s.game1918@gmail.com>
2021-10-18 11:59:12 +0900

    Routing追加

--- server.js
+++ server.js
@@ -1,26 +1,55 @@
 const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
- 
-var server = http.createServer();
-server.on('request', doRequest);
- 
+
 // ファイルモジュールを読み込む
 var fs = require('fs');
- 
+
+// ファイル読み込み
+const indexPage = fs.readFileSync('./index.html', 'UTF-8');
+const bootstrapCss = fs.readFileSync('./css/bootstrap.css', 'UTF-8');
+const jqueryCsvJs = fs.readFileSync('./jquery.csv.js', 'UTF-8');
+const readcsvJs = fs.readFileSync('./readcsv.js', 'UTF-8');
+const wirteJs = fs.readFileSync('./write.js', 'UTF-8');
+
+const url = require('url');
+
+var server = http.createServer(routeSetting);
+
 // リクエストの処理
-function doRequest(req, res) {
-    
-    // ファイルを読み込んだら、コールバック関数を実行する。
-    fs.readFile('./index.html', 'utf-8' , doReard );
-    
-    // コンテンツを表示する。
-    function doReard(err, data) {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        res.end();
+function routeSetting(req, res) {
+    const url_parts = url.parse(req.url)
+    switch(url_parts.pathname) {
+        case '/':
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(indexPage);
+            res.end();
+            break;
+        
+        case '/css/bootstrap.css':
+            res.writeHead(200, {'Content-Type': 'text/css'});
+            res.write(bootstrapCss);
+            res.end();
+            break;
+
+        case '/jquery.csv.js':
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.write(jqueryCsvJs);
+            res.end();
+            break;
+
+        case '/readcsv.js':
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.write(readcsvJs);
+            res.end();
+            break;
+        
+        case '/write.js':
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.write(wirteJs);
+            res.end();
+            break;
     }
-    
 }
  
 server.listen(port, hostname, () => {
